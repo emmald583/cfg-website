789834106a5b16dae8ce54403e0918d9f9ceb81c
emmald583 <56300670+emmald583@users.noreply.github.com>
2019-11-26 20:02:35 +0000

    Added changing text with click

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function quack () {
     audio.play()
+    changingText.innerHTML = greeting
+    changingText.style.color = "black"
 }
 
 let imageClick = document.getElementById("travelduck")
 let audio = new Audio('quack.mp3')
+let greeting = "QUACK"
+let changingText = document.querySelector("h2")
 imageClick.addEventListener("click", quack)
